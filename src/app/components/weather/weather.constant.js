a56ad7546d80b1272b8875d74c91372632fcf3f3
Emil Cieslar <cieslar@webkreativ.cz>
2016-10-18 10:56:16 +0200

    Updated URL to serve https requests over 'middle man' since openweathermap doesn't support https in free mode

--- src/app/components/weather/weather.constant.js
+++ src/app/components/weather/weather.constant.js
@@ -1,7 +1,7 @@
 export default ngModule => {
 
     const WEATHER = {
-        API_URL: 'http://api.openweathermap.org/data/2.5/weather',
+        API_URL: 'https://weatherapi.emmil.cz/',
         APP_ID: 'a5406db6084c9bf40b2b1196b196e199'
     }
 
